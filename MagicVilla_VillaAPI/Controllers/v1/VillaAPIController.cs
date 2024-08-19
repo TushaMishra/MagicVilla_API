1d0f8e7955f43a7124571778d375fa1f3dacc99a
Tusha Mishra <tushamishra123@gmail.com>
2024-08-20 05:08:58 +0530

    Section 11 - Add Pagination to Response Header

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Text.Json;
 
 namespace MagicVilla_VillaAPI.Controllers.v1
 {
@@ -50,6 +51,9 @@ namespace MagicVilla_VillaAPI.Controllers.v1
                 {
                     villaList = villaList.Where(u => u.Name.ToLower().Contains(search.ToLower()));
                 }
+                Pagination pagination = new() { PageNumber = pageNumber, PageSize = pageSize };
+
+                Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(pagination));
                 _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
